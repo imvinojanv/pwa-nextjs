580afb79d81d12584bb83b28eac740e9773554f7
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2024-05-03 17:38:17 +0530

    Updated Next config ✅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,      // Enable React strict mode for improved error handling
+    swcMinify: true,            // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== 'development'       // Remove console.log in production
+    }
+};
+
+export default withPWA({
+    dest: "public",         // destination directory for the PWA files
+    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
+    register: true,         // register the PWA service worker
+    skipWaiting: true,          // skip waiting for service worker activation
+})(nextConfig);
